11c690e77c15a9ae16965a405baf7a67cae2e513
Imran691 <mimran691@gmail.com>
2023-03-12 10:19:00 +0500

    menu button added in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,9 +5,11 @@ import Menu from "../public/icons/menu.png";
 export default function Header() {
   return (
     <div className="flex bg-slate-100 text-lg py-4 sm:py-8 px-4 sm:px-16 text-stone-700 font-serif font-bold">
-      <div className="flex-grow hover:underline text-lg md:text-2xl">Muhammad Imran</div>
+      <div className="flex hover:underline text-lg md:text-2xl">
+        Muhammad Imran
+      </div>
 
-      <div className=" invisible sm:visible">
+      <div className="hidden sm:flex">
         <ul className="flex px-16 flex-wrap">
           <li className=" px-4 hover:underline ">Work</li>
           <li className=" px-4 hover:underline ">About me</li>
@@ -15,12 +17,10 @@ export default function Header() {
         </ul>
 
 
-        
-      {/* <div className="visible sm:hidden flex-shrink-0 rounded-full">
-        <Image src={Menu} alt="menu-icon" height={60} width={60} />
-      </div> */}
-      
-</div>
+      </div>
+      <div className="sm:hidden flex  rounded-full justify-end flex-1 pr-8">
+          <Image src={Menu} alt="menu-icon" height={60} width={60} />
+        </div>
     </div>
   );
 }
