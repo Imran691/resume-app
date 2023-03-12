ed4fbe8635c898def8774a8bad3911078d4b8286
Imran691 <mimran691@gmail.com>
2023-03-12 10:25:37 +0500

    nav bar adjusted

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,11 +5,11 @@ import Menu from "../public/icons/menu.png";
 export default function Header() {
   return (
     <div className="flex bg-slate-100 text-lg py-4 sm:py-8 px-4 sm:px-16 text-stone-700 font-serif font-bold">
-      <div className="flex hover:underline text-lg md:text-2xl">
+      <div className="flex flex-grow hover:underline text-lg md:text-2xl">
         Muhammad Imran
       </div>
 
-      <div className="hidden sm:flex">
+      <div className="hidden sm:flex justify-end">
         <ul className="flex px-16 flex-wrap">
           <li className=" px-4 hover:underline ">Work</li>
           <li className=" px-4 hover:underline ">About me</li>
@@ -18,7 +18,7 @@ export default function Header() {
 
 
       </div>
-      <div className="sm:hidden flex  rounded-full justify-end flex-1 pr-8">
+      <div className="sm:hidden flex  rounded-full justify-end flex-1 pr-2">
           <Image src={Menu} alt="menu-icon" height={60} width={60} />
         </div>
     </div>
